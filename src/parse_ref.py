d032fa9c32508707225a7b1b78cf80745a8f596d
E Baron <baron@ou.edu>
2015-03-09 19:47:38 -0500

    merges with Lisa

---
+++ src/parse_ref.py
@@ -0,0 +1,90 @@
+######################################################################
+## Filename:      parse_ref.py
+## Author:        Eddie Baron <baron@ou.edu>
+## Created at:    Fri Mar  6 10:55:05 2015
+## Modified at:   Mon Mar  9 14:43:52 2015
+## Modified by:   Eddie Baron <baron@ou.edu>
+## Description:   read ref.dat file and use for annotating plots
+######################################################################
+import numpy as N
+import pylab
+import my_funcs
+pylab.interactive(True)
+from matplotlib.ticker import AutoMinorLocator
+
+fields = [('wl','f8'),('gf','f8'),('z','i'),('istg','i'),('chi','f8')]
+ref = N.loadtxt("ref.dat",dtype=fields)
+
+synow_file = "fit02bjx.dat"
+model = N.loadtxt(synow_file)
+
+numref = 10
+an = [1,  2, 22,  8, 12, 14, 16, 20, 20, 26]
+ai = [0,  0,  0,  1,  1,  1,  1,  1,  0,  1]
+ions_used = [ z*100+istg for z,istg in zip(an,ai) ]
+ref_ions = []
+for i in xrange(N.size(ref['wl'])):
+  ref_ions.append(ref['z'][i]*100 + ref['istg'][i])
+
+ref_index = []
+for ion in ions_used:
+  ref_index.append(ref_ions.index(ion))
+
+
+pylab.interactive(True)
+
+# One can supply an argument to AutoMinorLocator to
+# specify a fixed number of minor intervals per major interval, e.g.:
+# minorLocator = AutoMinorLocator(2)
+# would lead to a single minor tick between major ticks.
+
+minorLocator   = AutoMinorLocator()
+
+golden = (pylab.sqrt(5)+1.)/2.
+
+
+figprops = dict(figsize=(8., 8./ golden ), dpi=128)    # Figure properties for single and stacked plots 
+# figprops = dict(figsize=(16., 8./golden), dpi=128)    # Figure properties for side by sides
+adjustprops = dict(left=0.15, bottom=0.1, right=0.90, top=0.93, wspace=0.2, hspace=0.2)       # Subp
+
+fig = pylab.figure(1,**figprops)   # New figure
+fig.clf()
+fig.subplots_adjust(**adjustprops)  # Tunes the subplot layout
+
+ax1 = fig.add_subplot(1, 1, 1)
+
+my_funcs.bold_labels(ax1)
+
+p1, = ax1.plot(model[:,0],model[:,1],linewidth=2.0)
+ax1.set_ylabel(r'$F_\lambda$',fontsize=14)
+ax1.set_xlabel(r'$\lambda\ (\AA)$', fontsize=14)
+# ax1.set_xlim([0.,60.])
+# ax1.set_ylim([10.**41.4,10.**43.5])
+# ax1.set_yscale('log')
+# ax1.legend([p1,p2,p3,p4],['Day 10','Day 15','Day 25','Day 50'],frameon=False)
+ax1.xaxis.set_minor_locator(minorLocator)
+
+pylab.tick_params(which='both', width=2)
+pylab.tick_params(which='major', length=7)
+pylab.tick_params(which='minor', length=4, color='r')
+
+#ax1.xaxis.grid(True,which='minor')
+ax1.xaxis.grid(True,which='both')
+
+
+wl_ref = []
+f_ref = []
+ymin,ymax = ax1.get_ybound()
+for i in ref_index:
+  wl_ref.append([10.*ref['wl'][i],10.*ref['wl'][i]])
+  f_ref.append([ymin,ymax])
+
+for x,y in zip(wl_ref,f_ref):
+  ax1.plot(x,y,lw=2)
+
+
+# figname = "example"
+# png_name=figname+".png"
+# eps_name=figname+".eps"
+# fig.savefig(png_name)
+# fig.savefig(eps_name)
