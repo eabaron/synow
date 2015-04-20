b874b05b380fb959da4b913847da1d271427a2a3
E Baron <baron@ou.edu>
2015-04-20 10:36:11 -0500

    updated ion id scripts

--- src/parse_ref.py
+++ src/parse_ref.py
@@ -2,10 +2,11 @@
 ## Filename:      parse_ref.py
 ## Author:        Eddie Baron <baron@ou.edu>
 ## Created at:    Fri Mar  6 10:55:05 2015
-## Modified at:   Tue Mar 10 09:11:37 2015
+## Modified at:   Tue Mar 10 13:17:07 2015
 ## Modified by:   Eddie Baron <baron@ou.edu>
 ## Description:   read ref.dat file and use for annotating plots
 ######################################################################
+from __future__ import print_function, division
 import numpy as N
 import pylab
 import my_funcs
@@ -25,7 +26,7 @@ ions_used = [ z*100+istg for z,istg in zip(an,ai) ]
 ref_ions = []
 for i in xrange(N.size(ref['wl'])):
   ref_ions.append(ref['z'][i]*100 + ref['istg'][i])
-
+  
 ref_index = []
 for ion in ions_used:
   ref_index.append(ref_ions.index(ion))
@@ -77,7 +78,9 @@ f_ref = []
 ymin,ymax = ax1.get_ybound()
 for i in ref_index:
   wl_ref.append([10.*ref['wl'][i],10.*ref['wl'][i]])
-  f_ref.append([ymin,ymax])
+  ihelp = N.abs(model[:,0]-10.*ref['wl'][i]).argmin()
+  yhelp = model[ihelp,1]
+  f_ref.append([ymin,yhelp])
 
 for x,y in zip(wl_ref,f_ref):
   ax1.plot(x,y,lw=2)
@@ -89,9 +92,26 @@ fields = [('Z','i'),('A','f8'),('Name','S13'),('sym','S4'),('MP','f8'),\
 # labels = N.loadtxt("periodic_table.dat",skiprows=1,delimiter=',',dtype=fields)  
 labels = N.genfromtxt("periodic_table.dat",skip_header=1,delimiter=',',dtype=None)  
 
-syms = list(labels['f3'])
-syms.replace(" ","")
+syms = []
+for x in labels['f3']:
+  syms.append(x.replace(" ",""))
 
+ref_Zs = []
+for z in labels['f0']:
+  ref_Zs.append(z)
+  
+sym_indices = []
+for z in an:
+  sym_indices.append(ref_Zs.index(z))
+
+spect_notation = ["I","II","III","IV","V","VI","VII","VIII","IX","X"]
+text_labels = []
+for i,j in enumerate(sym_indices):
+  help = syms[j] + " " + spect_notation[ai[i]]
+  text_labels.append(help)
+  
+for x,y,l in zip(wl_ref,f_ref,text_labels):
+  ax1.text(x[0],min(y[1]*1.08,ymax),l,fontsize=8)
 
 # figname = "example"
 # png_name=figname+".png"
